1752e5a23744832cb040d4fc39577d9d02e2830e
Sylvie-B <unsolo@hotmail.fr>
2021-12-21 17:48:00 +0100

    play / pause video, timeupdate

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+let playBtn = $('.player__button').first();
+
+console.log(player)
+
+playBtn.click(function (){
+    if(playBtn.text() === "►" ){
+        player.trigger('play');
+        playBtn.text("II");
+    }
+    else {
+        player.trigger('pause');
+        playBtn.text("►")
+    }
+
+})
+
+player.get(0).addEventListener('timeupdate', function (){
+    let t = (player.get(0).currentTime / 596.5) * 100;
+    console.log(player.get(0).currentTime);
+    progress.css('width', t + "%")
+})
+
